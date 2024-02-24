bfe126ca8dc08c1a1becbc493fe6b2a21ffe6a82
naturlicht <majorjoe@yandex.ru>
2024-02-24 15:22:35 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        double freq = 937.45;
+
+        double df = freq - 936.899;
+        int nCh = (int)(df / 0.2);
+
+        System.out.println("nCh = " + nCh);
+    }
+}
